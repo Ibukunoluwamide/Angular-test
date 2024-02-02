777ffacf38c3b8ed6cc23191ed80c8ac59d3ddc6
Alao Olamide Ezekiel <125860905+Ibukunoluwamide@users.noreply.github.com>
2024-02-02 14:02:33 +0100

    angular test

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -18,13 +18,19 @@ export class AdminLoginComponent {
     password: ""
   }
   alladmins: any = []
+  ngOnInit(){
+
+    this.alladmins = JSON.parse(localStorage['CW-admins'] || null)
+
+  }
   loginBtn() {
     if (this.admin.username && this.admin.password) {
-      this.alladmins = JSON.parse(localStorage['CW-admins'])
-      let findadmin = this.alladmins.find((item: any, index:number) => item.username == this.admin.username && item.password == this.admin.password )
-      if (findadmin) {
-        // console.log();
-        console.log(findadmin);        
+      if (this.alladmins) {
+        
+        let findadmin = this.alladmins.find((item: any, index:number) => item.username == this.admin.username && item.password == this.admin.password )
+        if (findadmin) {
+          // console.log();
+          console.log(findadmin);        
         localStorage.setItem("current_admin", JSON.stringify(findadmin));
         localStorage.setItem("currentAdmin_id", this.alladmins.indexOf(findadmin));
         this.msg="Login Successful!"
@@ -33,6 +39,14 @@ export class AdminLoginComponent {
           this.router.navigate(['/dashboard'])
         }, 2000);
       } else {
+        this.msg = "Incorrect username or password"
+        this.style = "alert alert-danger text-center"
+        setTimeout(() => {
+          this.msg = ""
+          this.style = ""
+        }, 2000);
+      }
+      }else {
         this.msg = "Incorrect username or password"
         this.style = "alert alert-danger text-center"
         setTimeout(() => {
